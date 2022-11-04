b8f04ac308a0dea24e0c0ca0d331c5e8f1e9fd56
ThibaultLocquet <thibaultlocquetweb@gmail.com>
2022-11-04 09:50:35 +0100

    End of the buttons

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Header = () => {
+    return (
+        <div>
+            <Icon/>
+        </div>
+    );
+};
+
+export default Header;
