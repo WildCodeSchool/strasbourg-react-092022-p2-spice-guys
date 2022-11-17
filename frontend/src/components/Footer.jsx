e539bd1c23d7b55cedf6f52a0f1e78f1c460cbbb
ThibaultLocquet <thibaultlocquetweb@gmail.com>
2022-11-17 11:47:04 +0100

    fix_footer_bugs

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,37 +1,74 @@
-import * as React from 'react';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { styled } from '@mui/material/styles';
+import * as React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { styled } from "@mui/material/styles";
 
-
-const Root = styled('div')(({ theme }) => ({
+const Root = styled("div")(({ theme }) => ({
   padding: theme.spacing(1),
-  [theme.breakpoints.down('sm')]: {
-    display: 'none',
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
   },
-
 }));
-const Footer = () => {
-
-  return ( 
+function Footer() {
+  return (
     <Root>
-    <div style={{backgroundColor: "#C2A835", position :  "absolute", bottom : "0", width : "100%"}} classname="main-footer">
-          <div style={{color:"white", display:"flex", justifyContent:"center", paddingBottom:"1rem", paddingTop:"1rem", cursor:"pointer", fontSize: "large"}}>
-              <TwitterIcon sx={{fontSize: "2rem"}}></TwitterIcon > <FacebookIcon sx={{fontSize: "2rem  "}}></FacebookIcon> <LinkedInIcon sx={{fontSize: "2rem  "}}></LinkedInIcon>
-          </div>
-          <div style={{display:"flex", justifyContent:"center", fontSize: "18px"}}>
-              <a href="/" style={{textDecoration:"none", fontSize: "18px",color: "white"}}>Mentions Légales</a>
-          </div>
+      <div
+        style={{ backgroundColor: "#C2A835", bottom: "0", width: "100%" }}
+        className="main-footer"
+      >
+        <div
+          style={{
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            paddingBottom: "1rem",
+            paddingTop: "1rem",
+            fontSize: "large",
+          }}
+        >
+          <TwitterIcon sx={{ fontSize: "2rem", cursor: "pointer" }} />{" "}
+          <FacebookIcon sx={{ fontSize: "2rem", cursor: "pointer" }} />{" "}
+          <LinkedInIcon sx={{ fontSize: "2rem", cursor: "pointer" }} />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            fontSize: "18px",
+          }}
+        >
+          <a
+            href="/"
+            style={{ textDecoration: "none", fontSize: "18px", color: "white" }}
+          >
+            Mentions Légales
+          </a>
+        </div>
 
-          <div style={{display:"flex", justifyContent:"center", paddingTop:"1 rem"}}className="row">
-            <p style={{fontSize: "18px",color: "white",textdecorationLine: "none"}}>
-              &copy;{new Date().getFullYear()} The spices Guys 
-            </p>
-          </div>
-    </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "1 rem",
+          }}
+          className="row"
+        >
+          <p
+            style={{
+              fontSize: "18px",
+              color: "white",
+              textdecorationLine: "none",
+              paddingTop: "18px",
+              paddingBottom: "18px",
+            }}
+          >
+            &copy;{new Date().getFullYear()} The spices Guys
+          </p>
+        </div>
+      </div>
     </Root>
-   );
+  );
 }
 
-export  default Footer; 
+export default Footer;
