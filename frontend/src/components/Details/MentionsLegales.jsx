424d052c167ca114485c1aafca92e661f65ce1ee
ThibaultLocquet <thibaultlocquetweb@gmail.com>
2022-11-22 18:45:50 +0100

    new branch with new page

---
+++ frontend/src/components/Details/MentionsLegales.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Typography from "src/theme/overrides/Typography";
+
+function MentionsLegales() {
+  return (
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "#fff79b",
+        height: "100vh  ",
+      }}
+    >
+      <h1 style={{ fontSize: "2remp " }}>MENTIONS LÉGALES</h1>
+
+      <Typography variant="body1">
+        style=
+        {{
+          width: "50%",
+        }}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel iusto
+        adipisci ut non saepe expedita id perferendis impedit suscipit aliquam
+        quod omnis fugiat reprehenderit rem explicabo quas, minus dolorem
+        possimus molestiae repellat? Aperiam voluptates ullam nesciunt quos
+        fugit error atque laudantium id aliquid praesentium at sint dolorum
+        deleniti delectus, tempore earum molestiae corporis asperiores unde,
+        aspernatur placeat rerum, dolore libero! Aperiam minima impedit facere
+        vero enim corporis similique animi facilis at consequuntur, a commodi
+        nesciunt nobis eligendi laboriosam tempora unde excepturi alias ratione.
+        Dolore, officiis nostrum. Ex ab iusto ad aspernatur! A ducimus, vitae
+        inventore excepturi earum eligendi consequuntur error ab dolor nesciunt
+        qui facere consequatur exercitationem voluptatibus beatae sequi nam in
+        nostrum suscipit corporis? Porro minus tempore eum? Perspiciatis maxime,
+        quas ipsam animi deserunt pariatur voluptate et distinctio atque
+        necessitatibus magnam beatae eius placeat suscipit est eligendi? Nam
+        recusandae officia ab commodi labore tempora eius praesentium repellat
+        fugit consequuntur voluptatem deleniti aliquid numquam consequatur
+        nostrum dignissimos excepturi repudiandae omnis voluptates facere quasi
+        velit, magni repellendus facilis. Eos voluptatem ducimus laudantium
+        molestias debitis beatae! Eligendi, fugit ad adipisci at omnis, atque
+        tempore voluptates numquam excepturi eius officiis veniam quo
+        consequatur, explicabo asperiores earum vitae ipsum provident! Adipisci
+        molestiae optio dolore similique accusamus ipsum impedit non enim? Ab
+        ducimus quo ipsum, veritatis consequatur qui! Qui ipsa dolorum, quo enim
+        repellendus rem quia suscipit reiciendis commodi tempore iure
+        voluptatibus, odio eius doloremque, neque beatae! Reprehenderit
+        voluptatem optio blanditiis modi temporibus ad consequatur debitis quasi
+        incidunt eius. Rem rerum neque earum reiciendis atque eius, quas saepe
+        aliquam autem sequi, perferendis cum eum porro ipsum in unde sunt,
+        dignissimos tenetur impedit. Excepturi, dolore at error expedita minima
+        ipsam, autem voluptas libero perspiciatis assumenda facere aliquam nemo,
+        sunt eos non ipsum temporibus ab ad voluptates enim dolorum quis rerum
+        necessitatibus minus! Beatae quasi, debitis mollitia incidunt velit,
+        cumque iusto nesciunt ratione id tenetur odit quod quisquam dolore
+        dignissimos deleniti natus. Reprehenderit, cum ipsum. Cupiditate placeat
+        dolor impedit facilis quaerat, ut illum ab qui? Sequi qui aliquam, iusto
+        ut modi soluta ratione voluptatem labore? Ex aspernatur, adipisci
+        laboriosam quod nesciunt quaerat excepturi suscipit est tempore quas
+        repellat repudiandae, similique vel obcaecati culpa alias eos veniam
+        neque quidem praesentium perspiciatis ipsum sunt itaque quae?
+        Perspiciatis omnis, in adipisci cum voluptate qui, delectus incidunt
+        corporis magnam saepe reprehenderit nesciunt earum, ab sapiente aliquam.
+        Alias consequatur ex, maxime voluptates sequi, dolores dolorum nesciunt
+        corrupti, accusamus nulla voluptatem.
+      </Typography>
+    </div>
+  );
+}
+
+export default MentionsLegales;
